7f242433ae6b5c3fb83f06700887a68ba5dbfdbd
Yannys-coder <ccyannys123@gmail.com>
2024-09-09 23:12:24 -0400

    first commit

---
+++ main1.py
@@ -0,0 +1,16 @@
+# Imput Values from user
+a = input("Enter The Value of a: ")
+b = input("Enter The Value of b: ")
+c = input("Enter The Value of c: ")
+
+#Time to Swap
+two = a
+twelve = c
+a = b
+b = c
+b = two
+c = twelve
+
+print("Value of a after swapping", a)
+print("Value of b after swapping", b)
+print("Value of c after swapping", c)
